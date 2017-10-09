ad2bdd83fca74980423b83fba5ef20599579dba7
Martin Dario Cernadas <martindario.cernadas@bbva.com>
2017-10-09 17:37:49 -0300

    Adaptado a Buenos Aires

---
+++ locate.py
@@ -0,0 +1,20 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyBhjxBzY5A14d2Q0C_UgGcxpnVpOFZMyyw')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('Av Cordoba 111, Ciudad Autonoma de Buenos Aires, Argentina')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((-34.5982940, -58.3699040))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Balcarce 50",
+                                     "Ciudad Autonoma de Buenos Aires, ARG",
+                                     mode="transit",
+                                     departure_time=now)
+
+print("OK!")
+
